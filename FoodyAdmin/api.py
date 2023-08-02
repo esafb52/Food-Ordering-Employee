9a95eb616bd1c632dfd7ba2b76d9c229a9faa3e7
alisharify <alisharifyofficial@gmail.com>
2023-08-02 10:46:55 +0330

    admin index charts done
    admin last week orders chart (Section and User) Chart are done

--- FoodyAdmin/api.py
+++ FoodyAdmin/api.py
@@ -2,11 +2,12 @@ import datetime
 
 from flask import request, jsonify
 from FoodyAdmin import admin
-
-from FoodyCore.utils import TimeStamp
 from FoodyAuth.AccessControl.decorators import admin_login_required
-
 from FoodyOrder.model import Order
+from FoodyCore.extension import db
+from FoodyAuth.model import Section, User
+
+
 
 @admin.route("/api/AllOrders/", methods=["GET"])
 @admin_login_required
@@ -41,9 +42,61 @@ def All_Orders_API():
         time = f + datetime.timedelta(days=each)
         temp = {}
         query = Order.query.filter(Order.OrderDate == time).count()
-        temp[str(time)] = str(time)
-        temp["data"] = query
+        temp[str(time)] = query
         data.append(temp)
 
+    return jsonify({"status":"success", "data": data}), 200
+
+
+@admin.route("/api/AllOrders/Sections/", methods=["GET"])
+@admin_login_required
+def All_Orders_Sections_API():
+    """
+    this view take two ISO format date and return all Orders(filtered by SectionsName) that
+    are between these two dates
+    """
+    Ftime = request.args.get("from", type=str, default=None)
+    Etime = request.args.get("end", type=str, default=None)
+
+    if not Ftime or not Etime:
+        return jsonify({}), 400
+
+    try:
+        f = datetime.datetime.strptime(Ftime,"%Y-%m-%dT%H:%M:%S.%fZ")
+        e = datetime.datetime.strptime(Etime,"%Y-%m-%dT%H:%M:%S.%fZ")
+    except ValueError:
+        return jsonify({"status":"failed", "error": "invalid date format"}), 400
+    except Exception as e:
+        print(e)
+        return jsonify({"status":"failed", "error": "invalid date format"}), 400
+
+    f = f.date()
+    e = e.date()
+
+    AllSections = [section for section in db.session.query(Section.id, Section.Name).distinct().all()]
+
+    data = []
+    for sectionID, sectionName in AllSections:
+        all_section_orders = Order.query.join(User, Order.UserID == User.id).filter(User.SectionID == sectionID)\
+            .filter(Order.OrderDate >= f).filter(Order.OrderDate <= e)\
+            .count()
+
+        temp = {}
+        temp["section_name"] = str(sectionName)
+        temp["orders_count"] = all_section_orders
+        data.append(temp)
 
     return jsonify({"status":"success", "data": data}), 200
+
+
+
+@admin.route("/api/All/Users/", methods=["GET"])
+@admin_login_required
+def All_Users_Info():
+    """
+    this view return an info about all user and how many user each section have
+    """
+
+    AllSections = db.session.query(Section.id, Section.Name).distinct().all()
+
+    
--- FoodyAdmin/private_static/js/index.js
+++ FoodyAdmin/private_static/js/index.js
@@ -1,7 +1,4 @@
-
-
-async function GetOrdersInfo(start, end){
-
+async function GetOrdersInfo(start, end){ // this function get a week ago orders info in whole app
     let response = await fetch(`/admin/api/AllOrders/?from=${start.toString()}&end=${end.toString()}`, {
         method:"GET",
         headers :{
@@ -13,19 +10,59 @@ async function GetOrdersInfo(start, end){
 }
 
 
+async function GetSectionOrdersInfo(start, end){ // this function get each section a week ago orders info in whole app
+
+    let response = await fetch(`/admin/api/AllOrders/Sections/?from=${start.toString()}&end=${end.toString()}`, {
+        method:"GET",
+        headers :{
+            "X-CSRFToken": document.querySelector("#token").value
+        }
+    })
+    let data = (await response).json()
+    return data
+}
+
+
 window.addEventListener("DOMContentLoaded", async (e)=>{
-    const today = moment().toISOString();
+    const Today = moment().toISOString();
     const PreWeek = moment().subtract(1, 'week').toISOString();
-    let response = await GetOrdersInfo(today, PreWeek)
+
+    let GetOrdersInfo_response = await GetOrdersInfo(Today, PreWeek);
+    let GetSectionOrdersInfo_response = await GetSectionOrdersInfo(Today, PreWeek);
 
     let xValues = []
     let yValues = []
-    console.log(response)
-    Array.from(response.data).forEach((value, index)=>{
+    for (const value of GetOrdersInfo_response.data) {
+        for (const valueElement in value) {
+            xValues.push(valueElement)
+            yValues.push(value[valueElement])
+        }
+    }
 
-        console.log(value)
+    create_chart(
+        document.querySelector("#last-week-orders"),
+        "line",
+        xValues,
+        yValues,
+        "سفارشات هفته اخیر در کل بخش ها",
+        [65,120,195]
+    )
 
-    })
+    xValues = []
+    yValues = []
+    for (const Value of GetSectionOrdersInfo_response.data) {
+        xValues.push(Value["section_name"])
+        yValues.push(Value["orders_count"])
+    }
+
+    create_chart(
+        document.querySelector("#one-week-orders-section"),
+        "line",
+        xValues,
+        yValues,
+        "سفارشات هفته اخیر بر اساس بخش ها",
+        [65,120,195]
+    )
 
 
 })
